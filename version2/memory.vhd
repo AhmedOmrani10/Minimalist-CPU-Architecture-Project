f33600fdc2a11cda4208633242aedbb9726f7086
AhmedOmrani10 <119339974+AhmedOmrani10@users.noreply.github.com>
2025-10-08 23:25:54 +0200

    [DEV] Update memory

--- version2/memory.vhd
+++ version2/memory.vhd
@@ -4,22 +4,41 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity memory is
     Port (
-        addr_in_muxa : in  STD_LOGIC_VECTOR(11 downto 0);         
-        rnw  : in  STD_LOGIC;                             
-        data_out_memory : out STD_LOGIC_VECTOR(15 downto 0)          
+        
+        addr_in_muxa    : in  STD_LOGIC_VECTOR(11 downto 0); -- Address input
+        rnw             : in  STD_LOGIC;                     -- '1' = read, '0' = write
+        data_out_memory : inout STD_LOGIC_VECTOR(15 downto 0)  -- Data output
     );
 end memory;
 
 architecture Behavioral of memory is
-    type rom_array is array (0 to 65535) of STD_LOGIC_VECTOR(15 downto 0);
-    signal ROM : rom_array := (others => (others => '0'));  
-    signal temp_data : STD_LOGIC_VECTOR(15 downto 0);        
+    type ram_array is array (0 to 65535) of STD_LOGIC_VECTOR(15 downto 0);
+    signal RAM : ram_array := (
+        0 => x"0008",
+        1 => x"2009",
+        8 => x"0007",
+        9 => x"0005",
+        2 => x"1000",
+        3 => x"0000",
+        
+        others => (others => '0')
+    );
+
+    signal temp_data : STD_LOGIC_VECTOR(15 downto 0);
 begin
+
     process(addr_in_muxa)
     begin
-        temp_data <= ROM(to_integer(unsigned(addr_in_muxa)));
+        
+            if rnw = '0' then
+                
+                RAM(to_integer(unsigned(addr_in_muxa))) <= data_out_memory;
+            end if;
+            
+            temp_data <= RAM(to_integer(unsigned(addr_in_muxa)));
+       
     end process;
 
-    
     data_out_memory <= temp_data when rnw = '1' else (others => 'Z');
+
 end Behavioral;
